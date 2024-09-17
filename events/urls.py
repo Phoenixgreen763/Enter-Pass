369794199ca4f2167448b0e2e8491c3b3060239f
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-09-17 20:41:34 +0100

    update events.html to display events correctly.

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_events, name='all_events'),
+    path('all-events/', views.all_events, name='all_events'),
+    path('events/by-price/', views.events_by_price, name='events_by_price'),
+    path('events/by-rating/', views.events_by_rating, name='events_by_rating'),
+    path('events/category/<str:category>/', views.events_by_category, name='events_by_category'),
+    path('events/special/<str:special>/', views.events_special, name='events_special'),
 ]
