346963f14ded7e0617fab60a90bc242ecb652ed6
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-09-17 23:15:30 +0100

    add disable static to settings.py for development

--- enter_pass/settings.py
+++ enter_pass/settings.py
@@ -160,6 +160,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+DISABLE_COLLECTSTATIC=1
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
