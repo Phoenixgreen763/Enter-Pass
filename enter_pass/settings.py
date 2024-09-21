09c2b9d4e706886b7ff105403fe2e35bf7316bba
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-09-21 17:41:56 +0100

    collect all static files.

--- enter_pass/settings.py
+++ enter_pass/settings.py
@@ -160,8 +160,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-DISABLE_COLLECTSTATIC=1
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
