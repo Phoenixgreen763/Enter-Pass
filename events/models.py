5ccf4bd8cdb4542c0f66b753a7d22b0c694854b4
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-09-26 22:28:02 +0100

    add null field to total tickets in events model.

--- events/models.py
+++ events/models.py
@@ -12,7 +12,7 @@ class Event(models.Model):
     description = models.TextField()
     date = models.DateField()
     location = models.CharField(max_length=255)
-    total_tickets = models.PositiveIntegerField()
+    total_tickets = models.PositiveIntegerField(null=False)
     available_tickets = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Changed to ForeignKey
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
