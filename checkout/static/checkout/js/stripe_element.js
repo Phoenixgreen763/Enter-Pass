fb734b1730dfd0c4eef322a064936823dcd7f083
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-09-27 00:21:09 +0100

    add loading overlay and icon.

--- checkout/static/checkout/js/stripe_element.js
+++ checkout/static/checkout/js/stripe_element.js
@@ -74,6 +74,11 @@ form.addEventListener('submit', function(ev) {
                     email: $.trim(form.email.value),
                 }
             },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                email: $.trim(form.email.value),
+            },
         }).then(function(result) {
             if (result.error) {
                 var errorDiv = document.getElementById('card-errors');
