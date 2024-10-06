8d4269f9d0f849cbab66e60692af16d67e5193da
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-10-06 21:48:55 +0100

    include admin function to test coupons.

--- bag/admin.py
+++ bag/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Coupon
 
-# Register your models here.
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount_amount', 'expiration_date', 'usage_limit', 'used_count', 'is_valid')
+    search_fields = ('code',)
+
+admin.site.register(Coupon, CouponAdmin)
