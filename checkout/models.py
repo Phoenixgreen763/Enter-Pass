d243f25c4c88457aeacd3225a28898adbc73224d
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-10-08 19:00:53 +0100

    Fix AttributeError in OrderLineItem by replacing 'sku' with 'title' in __str__ method

--- checkout/models.py
+++ checkout/models.py
@@ -52,4 +52,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.event.sku} on order {self.order.order_number}'
+        return f'{self.event.title} on order {self.order.order_number}'
