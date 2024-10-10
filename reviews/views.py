0d440ba620485eb6ee4854678132e9cf7ebc64c6
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-10-10 20:39:00 +0100

    create view to display all user reviews.

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
 
-# Create your views here.
+def review_list(request):
+    reviews = Review.objects.all().order_by('-created_at')
+    form = ReviewForm()
+    
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user  # Associate the review with the logged-in user
+            review.save()
+            return redirect('review_list')  # Redirect to the reviews page after submitting
+
+    context = {
+        'reviews': reviews,
+        'form': form,
+    }
+    return render(request, 'reviews/review_list.html', context)
