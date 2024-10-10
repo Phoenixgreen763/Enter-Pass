067e30291ea182f664576121cde9edf8a3c5ad4f
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-10-10 20:53:32 +0100

    add the delete review url and button to form page.

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.review_list, name='review_list'),
     path('add/', views.add_review, name='add_review'),
+    path('delete/<int:review_id>/', views.delete_review, name='delete_review'),
 ]
