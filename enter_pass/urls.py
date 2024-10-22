0a0c5c79be6e79486f0181577e1173671f42d2eb
Phoenixgreen763 <148868559+Phoenixgreen763@users.noreply.github.com>
2024-10-23 00:41:36 +0100

    remove blank space for pep8

--- enter_pass/urls.py
+++ enter_pass/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, 
+    urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, 
+    urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
